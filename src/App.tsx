82da0e1780468d8cd4eda42a32d74081e695e28c
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-03-07 16:39:41 +0100

    added form logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,35 @@ import { useState } from "react";
 
 import { Form, WeatherData, WeatherDataWrapper } from "./components";
 
+export type Api = 'openweather' | 'visual';
+
 export interface IWeatherData {
   temperature: number,
   humidity: number,
   pressure: number
 }
 
+export interface IFormData {
+  lat: { value: number };
+  lon: { value: number };
+  api: { value: Api };
+}
+
+export interface IStateFormData {
+  lat: number;
+  lon: number;
+  api: Api;
+}
+
 function App() {
 
+  const [formData, setFormData] = useState<IStateFormData | null>(null);
   const [weatherData, setWeatherData] = useState<IWeatherData | null>(null);
 
   return (
     <div className="App">
       <main>
-        <Form />
+        <Form setFormData={setFormData} />
         <WeatherDataWrapper>
           {weatherData && <WeatherData data={(weatherData)} />}
         </WeatherDataWrapper>
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,41 @@
-import { APITypeInput, CoordinatesInput, Button } from "./atoms/";
+import { useState, useEffect } from "react";
 
-export const Form = () => {
+import { APITypeInput, CoordinatesInput, Button } from "./atoms/";
+import { Api, IFormData, IStateFormData } from '../App';
+import {validateCoordinates} from '../utils/validateCoordinates';
+
+interface FormProps {
+    setFormData: React.Dispatch<React.SetStateAction<IStateFormData | null>>
+}
+
+export const Form = ({ setFormData }: FormProps) => {
+
+    const [lat, setLat] = useState<number | null>(null);
+    const [lon, setLon] = useState<number | null>(null);
+    const [api, setApi] = useState<Api>("openweather");
+    const [msg, setMsg] = useState("");
+
+    useEffect(() => {
+        if (api && lat && lon) {
+            setFormData({ api, lat, lon });
+        }
+    }, [lat, lon, api])
+
+    const submitForm = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        let { lat, lon, api } = e.target as typeof e.target & IFormData;
+        if (validateCoordinates(lat.value,lon.value)) {
+            setLat(lat.value);
+            setLon(lon.value);
+            setApi(api.value);
+            setMsg("");
+        } else {
+            setMsg('Please fill correctly all the fields.');
+        }
+    }
 
     return (
-        <form className="Form">
+        <form className="Form" onSubmit={e => submitForm(e)}>
             <div className="Form__inputs">
                 <CoordinatesInput name={"lat"} />
                 <CoordinatesInput name={"lon"} />
@@ -13,7 +45,7 @@ export const Form = () => {
                 <APITypeInput value={"visual"} label={"Visual Crossing API"} defaultChecked={false} />
             </div>
             <Button id={"submit"} label={"check"} />
-            <small></small>
+            <small className="submit-message">{msg}</small>
         </form>
     )
 }
