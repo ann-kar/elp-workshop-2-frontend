635d0847fc0c7dcd5e2ce8af2690de862cb0cce7
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-03-07 21:52:47 +0100

    added validateCoordinates() to helpers

--- src/helpers/helperFunctions.ts
+++ src/helpers/helperFunctions.ts
@@ -26,5 +26,8 @@ export const helperFunctions = {
       .catch(err => {
         throw new Error(err)
       })
+  },
+  validateCoordinates: (lat: number, lon: number) => {
+    return (lat >= -90 && lat <= 90 && lon >= -180 && lon <= 180) ? true : false
   }
 }
