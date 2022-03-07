068d06d3747d392340a90cb3d2751b556fde9fbd
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-03-07 22:43:46 +0100

    added separate spans for weather property values and units

--- src/components/WeatherData.tsx
+++ src/components/WeatherData.tsx
@@ -4,13 +4,13 @@ export const WeatherData = ({ data }: { data: IWeatherData }) => {
     return (
         <div className="WeatherData">
             <div className="temperature">
-                Temperature: <span id="temperature">{data.temperature}<span>&#8451;</span></span>
+                Temperature: <span id="temperature">{data.temperature}</span><span>&#8451;</span>
             </div>
             <div className="pressure">
-                Pressure: <span id="pressure">{data.pressure} hP</span>
+                Pressure: <span id="pressure">{data.pressure}</span><span>&nbsp;hP</span>
             </div>
             <div className="humidity">
-                Humidity: <span id="humidity">{data.humidity}%</span>
+                Humidity: <span id="humidity">{data.humidity}</span><span>%</span>
             </div>
         </div>
     )
